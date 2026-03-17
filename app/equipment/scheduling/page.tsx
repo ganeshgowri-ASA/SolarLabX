3a01a19fb9ecc449f238b5774a684afc14fe5ec4
ASA <ganeshgowri.mitsui@gmail.com>
2026-03-17 06:06:47 +0530

    feat: add equipment scheduling page route

---
+++ app/equipment/scheduling/page.tsx
@@ -0,0 +1,5 @@
+import EquipmentSchedulingDashboard from "@/components/equipment/EquipmentSchedulingDashboard";
+
+export default function EquipmentSchedulingPage() {
+  return <EquipmentSchedulingDashboard />;
+}
